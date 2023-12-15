a15a4eb6fbff5830d0e0c6e7aa6f339f4459e7f3
4clover88y <4clover88y@naver.com>
2023-12-15 21:51:32 +0900

    231215_03

--- views/test/formtest.js
+++ views/test/formtest.js
@@ -8,21 +8,22 @@ function addBox (x) {
     // NewDiv 생성
     var newDiv = document.createElement('div');
     newDiv.id = 'box' + num++;
+    newDiv.className = 'happy';
     asdf.appendChild(newDiv);
     newDiv.style.border = '1px solid black';
-    newDiv.appendChild(document.createElement('p'));
 
     // 요소 확인용
-    var newP = document.createElement('p');
-    newP.innerHTML = "만들엉";
-    newP.id = 'checkP';
-    newDiv.appendChild(newP);
+    // var newP = document.createElement('p');
+    // newP.innerHTML = "만들엉";
+    // newP.id = 'checkP';
+    // newDiv.appendChild(newP);
 
     // NewDiv에 재료 이름 입력칸 만들기
     var newArea = document.createElement('input');
     newArea.type = 'text'
     newArea.name = 'ingredient';
-    newArea.placeholder = newDiv.id;
+    newArea.id = 'I'+i;
+    newArea.placeholder = newArea.id;
     newArea.required = true;
     newDiv.appendChild(newArea);
 
@@ -30,7 +31,8 @@ function addBox (x) {
     var newArea2 = document.createElement('input');
     newArea2.type = 'text'
     newArea2.name = 'quantity';
-    newArea2.placeholder = newDiv.id;
+    newArea2.id = 'Q'+i;
+    newArea2.placeholder = newArea2.id;
     newArea2.required = true;
     newDiv.appendChild(newArea2);
 
@@ -54,27 +56,93 @@ function addBox (x) {
 
 }
 
-var send = document.getElementById("send");
-send.addEventListener("click", function () {
-    var form = document.getElementById("form");
-    // var id = document.getElementById("id");
-    // var pw = document.getElementById("pw");
+var first = 0;
 
-    if (document.querySelector('#checkP') !== null) {
-        alert("있당");
+function deleteFirst() {
+    if(confirm('해당 재료를 삭제하시겠습니까?')){
+        if (document.querySelector('.happy') == null ) {
+            alert('더이상 삭제할 수 없습니다!');
+        }
+        else {
+    
+            // alert(document.getElementById('Q'+first).value);
+    
+            for (var cnt = 1; cnt<99; cnt++){
+                var bDiv = document.getElementById('box'+cnt);
+                if (bDiv !== null){
+    
+                    document.getElementById('I'+first).value = document.getElementById('I'+cnt).value;
+                    document.getElementById('Q'+first).value = document.getElementById('Q'+cnt).value;
+    
+                    first = cnt;
+                }
+            }
+        }
+        
+        const delBoxf = document.getElementById('box' + first);
+        delBoxf.remove();
+    
+        first = 0;
     }
-    else {
-        alert("없당");
-        return false;
-    }
+}
+
+// var send = document.getElementById("send");
+// send.addEventListener("click", function () {
+//     var form = document.getElementById("form");
+//     // var id = document.getElementById("id");
+//     // var pw = document.getElementById("pw");
+
+//     // var abc = document.getElementsById("ingredient");
+//     // var def = document.getElementsByName("quantity");
+
+    
+
+//     // 재료 하나도 없을 때
+//     if (document.querySelector('#checkP') == null) {
+//         alert("없당");
+//         return false;
+//     }
+
+//     // 유효성 검사
+//     // if (abc.value.trim() == "") {
+//     //     alert('아이디를 입력해주세여');
+//     //     // document.getElementById("id").focus();
+//     //     return false;
+//     // }  
+
+//     var cf = confirm("제출하시겠습니까?");
+//     if(cf){
+//         form.action = "http://www.naver.com";
+//         form.mothod = "GET";
+//         form.submit();
+//     }
+
+
+
+    //     if(abc.value.length !== 0 && def.value.length !== 0) {
+    //         // 제출하시겠습니까? 물어보기
+    //         var cf = confirm("제출하시겠습니까?");
+    //         if(cf){
+    //             form.action = "http://www.naver.com";
+    //             form.mothod = "GET";
+    //             form.submit();
+    //         }
+    //     }
+    //     else {
+    //         alert('뭐가문제야');
+    //         form.action = "http://www.naver.com";
+    //         form.mothod = "GET";
+    //         form.submit();
+    //     }
+    // }
+    
+
 
     // if (pw.value.trim() == "" || id.value.trim() == "") {
     //     alert("id와 비번 잘 적어라");
     //     return false;
     // }
 
-    form.action = "http://www.naver.com";
-    form.mothod = "GET";
-    form.submit();
 
-});
+
+// });
