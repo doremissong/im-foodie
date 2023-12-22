b9d58d93dd9ed756e54b657faca995607287b262
MISONG <zelly1020@naver.com>
2023-12-22 15:29:05 +0900

    231222_1_merge1

---
+++ public/js/gatherImadeClick.js
@@ -0,0 +1,36 @@
+$(".imade-block-btn").click(function(){
+    var $CI = $('.imade-block-btn').index(this);
+    // console.log($('.imade-block-btn').index(this));
+    // console.log($CI);
+    // console.log($('.imade-block-btn')[$CI]);
+
+    $($('.imade-contents-none')[$CI]).css({
+        display : 'block'
+    });
+
+    $($('.imade-block-btn')[$CI]).css({
+        display : 'none'
+    });
+
+    $($('.imade-none-btn')[$CI]).css({
+        display : 'block'
+    });
+
+})
+
+$(".imade-none-btn").click(function(){
+    var $DI = $('.imade-none-btn').index(this);
+
+    $($('.imade-contents-none')[$DI]).css({
+        display : 'none'
+    });
+
+    $($('.imade-block-btn')[$DI]).css({
+        display : 'block'
+    });
+
+    $($('.imade-none-btn')[$DI]).css({
+        display : 'none'
+    });
+
+})
