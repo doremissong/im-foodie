783eb748fa19050712858893cd2d5fcebdde35d5
MISONG <zelly1020@naver.com>
2023-12-27 02:16:51 +0900

    231227_3

--- public/js/checkPassword.js
+++ public/js/checkPassword.js
@@ -1,6 +1,15 @@
+function checkAccord() {
+    if (document.getElementById("newPW").value == document.getElementById("checkNewPW").value) {
+        document.getElementById("notAccord").style.display = 'none';
+    }
+    else{
+        document.getElementById("notAccord").style.display = 'block';
+    }
+}
+
 function checkPW() {
     if (document.getElementById("newPW").value != document.getElementById("checkNewPW").value) {
-        alert("aa");
+        alert("입력하신 새로운 비밀번호가 다릅니다! 다시 입력해주세요.");
         return false;
     }
     else {
